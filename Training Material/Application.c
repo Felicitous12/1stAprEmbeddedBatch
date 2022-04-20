04e415dcf79f6d8479b987437aa2f071e921ac75
Sharada1998 <sharadashru98@gmail.com>
2022-04-20 14:30:04 +0530

    Training Documents

---
+++ Training Material/Application.c
@@ -0,0 +1,59 @@
+
+
+///////////////////////////////////////// INPUT //////////////////////////////////////////////////////////////
+/*Information*/
+unsigned char G_Eng_EngineTemperature_uchar = 50u ;
+unsigned int G_Eng_EngineRpm_uint = 1000u;
+signed int G_Str_SteeringAngle_sint = -60;
+
+/********************** DO NOT MODIFY BELOW SECTION **********************************************************/
+
+/*Message*/
+unsigned char G_Msg_SteeringInformation_Byte[3u];
+unsigned char G_Msg_EngineInformation_Byte[5u];
+
+unsigned char GetEngineTemperature(void);
+unsigned int GetEngineRPM(void);
+signed int GetSteeringRotation(void);
+
+
+
+unsigned char GetEngineTemperature(void)
+{
+	/*Every increment in LSB value increases 1 deg (i.e. return value 1 represents 1deg */
+	/*Range of output is 0deg to 255deg C*/
+	return G_Eng_EngineTemperature_uchar;
+}
+
+unsigned int GetEngineRPM(void)
+{
+	/*Every increment in LSB value increases 1 rpm (i.e. return value 1 represents 1rpm */
+
+	/*Range of output is 0 rpm to 8192 rpm*/
+	return G_Eng_EngineRpm_uint;
+
+}
+
+signed int GetSteeringRotation(void)
+{
+	/*Every increment in LSB value increases 1 deg (i.e. return value 1 represents 1deg and return value -1 represents -1 deg */
+	/*Range of output is -720 deg to +720 deg angle */
+	return G_Str_SteeringAngle_sint;
+}
+
+/********************** DO NOT MODIFY UNTILL HERE**********************************************************/
+
+
+void Com_PackEngineInformation(unsigned char *Byte_uchar)
+{
+	/*Type your code here*/
+	
+
+}
+
+void Com_PackSteeringInformation(unsigned char *Byte_uchar)
+{
+	/*Type your code here*/
+	
+}
+
